11737803cff2a83f0a86321038afa3b2f25967d5
Ogundeinde Adedamola <adedamola21@yahoo.com>
2017-07-05 04:34:50 +0000

    made user page a drop down menu

--- resources/views/partial/user-header.blade.php
+++ resources/views/partial/user-header.blade.php
@@ -5,16 +5,24 @@
             <div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
                 <a href="{{ route('homepage') }}" class="tm-site-name">Holiday</a>	
             </div>
-            <div class="col-lg-6 col-md-8 col-sm-9">
-                <div class="mobile-menu-icon">
+            <div class="collapse navbar-collapse tm-nav">
+                <!--<div class="mobile-menu-icon">
                     <i class="fa fa-bars"></i>
-                </div>
-                <nav class="tm-nav">
-                    <ul>
-                        <li><a href="{{ route('home') }}" class="active">Dashboard</a></li>
-                        <li><a href="{{ route('login') }}">Book a room</a></li>
-                        <li><a href="#" id='logout-url'>Logout</a></li>
-                    </ul>
+                </div>-->
+                <nav class="navbar-right navbar-nav navbar-right">
+                    <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->first_name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ route('login') }}" class="active">Dashboard</a></li>
+                                    <li><a href="{{ route('login') }}">Manage Bookings</a></li>
+                                    <li><a href="{{ route('login') }}">Manage Events</a></li>
+                                    <li><a href="{{ route('login') }}">Add Events</a></li>
+                                    <li><a href="#" id='logout-url'>Logout</a></li>
+                                </ul>
+                            </li>
                     <form method="post" action="{{ route('logout') }}" id='logout-form'>
                         {{ csrf_field() }}
                     </form>
@@ -23,3 +31,4 @@
         </div>
     </div>	  	
 </div>
+
